a5b1a6208db9959f79e29bc274c4e83657a47a45
Aaron <awoo452@wgu.edu>
2022-12-16 13:40:12 -0800

    IDK man

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let summedArray = 0;
+	for (let i = 0; i < array.length; i++) {
+    summedArray += array[i];
+  }
+  return summedArray;
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return (a * b);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let result = a;
+    if (a === 0 || a === 1) 
+      return 1; 
+    while (a > 1) { 
+      a--;
+      result *= a;
+    }
+    return result;
 };
 
 // Do not edit below this line
