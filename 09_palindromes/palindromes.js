625173921e60af6a02b14982472d4553ea133daf
Aaron <awoo452@wgu.edu>
2022-12-20 18:54:54 -0500

    Palindromes works badly

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let flippedArray = [];
+    let array = [];
+    for (let i = string.length - 1; i >= 0; i--) {
+        flippedArray.push(string[i]);
+    }
+    for (let j = 0; j < string.length; j++) {
+        array.push(string[j]);
+    }
+    console.log(flippedArray);
+    console.log(array);
+    for (let k = 0; k < string.length; k++) {
+    if (array[k] === flippedArray[k]) {
+            return true;
+        } else return false;
+    }
 };
 
 // Do not edit below this line
