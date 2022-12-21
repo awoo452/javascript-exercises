92a579ba393eb81bbebfa073b39fa77d9a28d304
Aaron <awoo452@wgu.edu>
2022-12-20 20:00:06 -0500

    getTheTitties works

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(books) {
+    let newArray = [];
+    for (let i = 0; i < books.length; i++) {
+    newArray.push(books[i].title);
+    }
+    return newArray;
 };
 
 // Do not edit below this line
