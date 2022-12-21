62d459988081e40c0c66145343f1d4a327323ec7
Aaron <awoo452@wgu.edu>
2022-12-20 19:52:46 -0500

    Fibonacci completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(nth) {
+    if (nth < 0) {
+        return 'OOPS';
+    } else {
+    let i;
+    let fib = [0, 1];
+    for (i = 2; i <= nth; i++) {
+    fib[i] = fib[i - 2] + fib[i - 1];
+    }
+    return fib[nth];
+    }
 };
 
 // Do not edit below this line
